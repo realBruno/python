fb8106dde4292b798f9230add79ebac7efd6a296
realBruno <soubruno@protonmail.com>
2024-10-23 09:27:39 -0300

    22/10/2024

--- area51.py
+++ area51.py
@@ -1,1 +1,3 @@
-print(91**8)
+nome = ['B', 'r', 'u', 'n', 'o', ' ', 'F', 'e', 'r', 'n', 'a', 'n', 'd', 'e', 's']
+print(nome)
+nome = map(str, nome)
---
+++ problemas/beecrowd/3strings/1encryptionv2.py
@@ -0,0 +1,23 @@
+# Encryption (v2)
+# 22/10/2024
+
+palavras = []
+for i in range(int(input())):
+    palavra, formatada = input(), ''
+    
+    for letra in palavra:
+        if ord(letra) in range(65, 91) or ord(letra) in range(97, 123):
+            formatada += chr(ord(letra) + 3)
+        else:
+            formatada += letra
+    
+    formatada = formatada[::-1]
+    palavra = formatada[:len(formatada)//2]
+
+    for letra in formatada[len(formatada)//2:]:
+        palavra += chr(ord(letra) - 1)
+    
+    palavras.append(palavra)
+
+for palavra in palavras:
+    print(palavra)
