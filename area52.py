e4bd39b0e0e21918f52acf44ce5ffc9cebaa5d59
realBruno <soubruno@protonmail.com>
2024-11-01 09:55:38 -0300

    01/11/2024

--- area52.py
+++
@@ -1,9 +0,0 @@
-sentence = input()
-dancing = ''
-for i in range(len(sentence)):
-    if i % 2 == 0:
-        dancing += sentence[i].upper()
-    else:
-        dancing += sentence[i].lower()
-
-print(dancing)
